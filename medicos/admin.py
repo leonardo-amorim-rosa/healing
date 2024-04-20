2ff224ac26114608b53ff09b70ef0222fe6c16a5
Leonardo Rosa <leonardo.rosa@akross.com.br>
2024-04-20 20:08:01 -0300

    doctor save implementation

---
+++ medicos/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Especialidade, DadosMedico, DatasAbertas
+
+
+admin.site.register(Especialidade)
+admin.site.register(DadosMedico)
+admin.site.register(DatasAbertas)
+
---
+++ medicos/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+def is_medico(user):
+    return DadosMedico.objects.filter(user=user).exists()
+
+
+class DatasAbertas(models.Model):
+    data = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    agendado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.data)
+
+
+class Especialidade(models.Model):
+    especialidade = models.CharField(max_length=100)
+    icone = models.ImageField(upload_to="icones", blank=True, null=True)
+
+    def __str__(self):
+        return self.especialidade
+
+
+class DadosMedico(models.Model):
+    crm = models.CharField(max_length=30)
+    nome = models.CharField(max_length=100)
+    cep = models.CharField(max_length=15)
+    rua = models.CharField(max_length=100)
+    bairro = models.CharField(max_length=100)
+    numero = models.IntegerField()
+    rg = models.ImageField(upload_to="rgs")
+    cedula_identidade_medica = models.ImageField(upload_to='cim')
+    foto = models.ImageField(upload_to="fotos_perfil")
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    descricao = models.TextField(null=True, blank=True)
+    especialidade = models.ForeignKey(Especialidade, on_delete=models.DO_NOTHING, null=True, blank=True)
+    valor_consulta = models.FloatField(default=100)
+
+    def __str__(self):
+        return self.user.username    
