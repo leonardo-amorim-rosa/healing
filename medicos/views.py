cfe552d09846dfcfa10061f53d222d4eee9c24c7
Leonardo Rosa <leonardo.rosa@akross.com.br>
2024-04-21 22:51:06 -0300

    doctor schedulings

--- medicos/views.py
+++ medicos/views.py
@@ -1,10 +1,10 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.messages import constants
 from medicos.models import DadosMedico, Especialidade, DatasAbertas, is_medico
-
+from pacientes.models import Consulta
 
 @login_required
 def abrir_horario(request):
@@ -85,3 +85,16 @@ def cadastro_medico(request):
 
         messages.add_message(request, constants.SUCCESS, 'Cadastro médico realizado com sucesso!')
         return redirect('/medicos/abrir_horario')
+
+@login_required
+def consultas_medico(request):
+    if not is_medico(request.user):
+        messages.add_message(request, constants.WARNING, 'Somente médicos podem acessar essa página.')
+        return redirect('/usuarios/sair')
+    
+    hoje = datetime.now().date()
+
+    consultas_hoje = Consulta.objects.filter(data_aberta__user=request.user).filter(data_aberta__data__gte=hoje).filter(data_aberta__data__lt=hoje + timedelta(days=1))
+    consultas_restantes = Consulta.objects.exclude(id__in=consultas_hoje.values('id'))
+
+    return render(request, 'consultas_medico.html', {'consultas_hoje': consultas_hoje, 'consultas_restantes': consultas_restantes, 'is_medico': is_medico(request.user)})
