c5ebd9a1c9b7720654b5c9cb783f8d38aa0d2762
Leonardo Rosa <leonardo.rosa@akross.com.br>
2024-04-27 22:26:23 -0300

    Add root path to login

--- healing/urls.py
+++ healing/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -24,4 +25,5 @@ urlpatterns = [
     path('usuarios/', include('usuarios.urls')),
     path('medicos/', include('medicos.urls')),
     path('pacientes/', include('pacientes.urls')),
+    path('', lambda request: redirect('/usuarios/login/', permanent=True)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
